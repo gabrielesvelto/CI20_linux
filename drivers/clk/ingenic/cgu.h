c722d8e47e843f0dbc59b737ebc55213f406bf95
Dragan Cecavac <dragan.cecavac@mips.com>
2018-03-21 15:17:03 +0100

    clk: ingenic: Fix jz4780 MSC divider calculation
    Change-Id: I70c7eec13dae4ec86f08261c0be1282b3fc1c869
    Signed-off-by: Dragan Cecavac <dragan.cecavac@mips.com>
    Signed-off-by: Darko Ristivojevic <darko.ristivojevic@mips.com>

--- drivers/clk/ingenic/cgu.h
+++ drivers/clk/ingenic/cgu.h
@@ -78,6 +78,9 @@ struct ingenic_cgu_mux_info {
  * @reg: offset of the divider control register within the CGU
  * @shift: number of bits to shift the divide value by (ie. the index of
  *         the lowest bit of the divide value within its control register)
+ * @div: number of bits to divide the divider value by (i.e. if the
+ *       effective divider value is the value written to the register
+ *       multiplied by some constant)
  * @bits: the size of the divide value in bits
  * @ce_bit: the index of the change enable bit within reg, or -1 if there
  *          isn't one
@@ -87,6 +90,7 @@ struct ingenic_cgu_mux_info {
 struct ingenic_cgu_div_info {
 	unsigned reg;
 	u8 shift;
+	u8 div;
 	u8 bits;
 	s8 ce_bit;
 	s8 busy_bit;
